bc048b4b74c4fa9ee9f9c4805c65eb5170c9d88f
arzatf <arzamansoori786@gmail.com>
2022-10-21 13:30:33 +0530

    first commit

---
+++ readme.md
@@ -0,0 +1,29 @@
+![COUNTDOWN](https://img.shields.io/badge/PROJECT-COUNTDOWN-%23D291BC)
+
+Stopwatch website is created with using HTML, CSS(styling yet to be done) and JavaScript.
+
+## TABLE OF CONTENTS
+#
+* General Info
+* Technologies
+* Launch
+
+### General Info
+#
+Countdown Website helps you to set the timer in seconds. Lets you reset and stop the timer at the same time.
+
+### Technologies
+#
+* HTML
+* CSS(yet to be done)
+* JavaScript
+
+### Launch
+#
+To use Countdown website or to see the project. Please click on the deployed link below.
+
+[COUNTDOWN](https://stopwatchwebsite.netlify.app/)
+<br>
+<br>
+<!-- ![STOPWATCH Website Image](./stopwatch.png) -->
+
---
+++ script.js
@@ -0,0 +1,38 @@
+const val = document.getElementById("number");
+const btn = document.getElementById("start");
+const stopE = document.getElementById("stop");
+const reset = document.getElementById("reset")
+const para = document.getElementById("count");
+let para1;
+let help;
+
+//taking value from user
+function setCount(){
+    if(val.value > 0){
+        para.innerHTML = val.value;
+        para.innerHTML = val.value--;
+    }
+}
+
+//starting interval
+function startCount(){
+     help = setInterval(setCount, 1000)
+}
+
+//start button
+btn.addEventListener("click", () => {
+    val.style.display = "none";
+    startCount();
+})
+
+//stop button
+stopE.addEventListener("click", ()=>{
+    clearInterval(help);
+})
+
+//reset button
+reset.addEventListener("click",()=>{
+    val.value= " ";
+    val.style.display = "inline";
+})
+
