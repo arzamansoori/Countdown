7b846fa05b6b8fd65537aac5f2902227dc780448
arzatf <arzamansoori786@gmail.com>
2022-11-06 22:27:54 +0530

    fixing logic of reset button

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ const btn = document.getElementById("start");
 const stopE = document.getElementById("stop");
 const reset = document.getElementById("reset")
 const para = document.getElementById("count");
+const middleP = document.getElementsByClassName("middle");
+
 let para1;
 let help;
 
@@ -28,11 +30,19 @@ btn.addEventListener("click", () => {
 //stop button
 stopE.addEventListener("click", ()=>{
     clearInterval(help);
+    const reset = document.createElement("button");
+    const resetTNode = document.createTextNode("RESET");
+    reset.appendChild(resetTNode)
+    reset.classList.add("btn");
+    middleP[0].appendChild(reset);
+
+    reset.addEventListener("click",()=>{
+        val.value= " ";
+        // para.innerHTML = 0;
+        val.style.display = "inline";
+        reset.style.display = "none";
+    })
+    
 })
 
-//reset button
-reset.addEventListener("click",()=>{
-    val.value= 0;
-    val.style.display = "inline";
-})
 
